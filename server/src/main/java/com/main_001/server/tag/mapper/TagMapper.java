30d2867220b5abdab248cb4f7fb74f791cdb485d
johnny98 <ishien98@gmail.com>
2023-01-20 01:56:43 +0900

    TagController, TagService 구현

--- server/src/main/java/com/main_001/server/tag/mapper/TagMapper.java
+++ server/src/main/java/com/main_001/server/tag/mapper/TagMapper.java
@@ -1,7 +1,15 @@
 package com.main_001.server.tag.mapper;
 
+import com.main_001.server.tag.dto.TagDto;
+import com.main_001.server.tag.entity.Tag;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper {
+    Tag tagPostDtoToTag(TagDto.Post tagDto);
+    TagDto.Responce TagToTagResponseDto(Tag tag);
+    Page<TagDto.Responce> TagsToTagResponseDtos(List<Tag> tag);
 }
--- server/src/main/java/com/main_001/server/tag/service/TagService.java
+++ server/src/main/java/com/main_001/server/tag/service/TagService.java
@@ -1,7 +1,25 @@
 package com.main_001.server.tag.service;
 
+import com.main_001.server.tag.entity.Tag;
+import com.main_001.server.tag.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class TagService {
+    private final TagRepository tagRepository;
+
+    public Tag createTag(Tag tag){
+        return tagRepository.save(tag);
+    }
+    public List<Tag> findTagsRecruit(){
+        return tagRepository.finndAllbyRcruitTag();
+    }
+
+    public List<Tag> findTagsFreeboard(){
+        return findTagsFreeboard();
+    }
 }
