fcfd46a4e48e2d90c294274f23cc344cf31721d3
himmel37 <dahamong37@gmail.com>
2023-01-25 13:20:41 +0900

    update: 체크박스가 체크되지 않던 오류 수정

--- client/src/components/Tag.tsx
+++ client/src/components/Tag.tsx
@@ -2,30 +2,30 @@ import styled from 'styled-components';
 
 const TagContainer = styled.span`
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
   text-align: center;
   border: 1px solid white;
   border-radius: 0.3rem;
-  width: 6.55rem;
-  height: 2.5rem;
+  /* width: 6.55rem; */
+  width: 7rem;
+  height: 3rem;
   margin: 0.05rem;
 
   input {
-    width: 0.8rem;
     bottom: 0.5rem;
     margin-top: 0.75rem;
-    /* bottom:10px; */
     top: 0.25rem;
+    flex: 1;
   }
 
   label {
-    width: 5rem;
+    /* width: 6rem; */
     font-size: 10px;
-    padding: 0.25rem;
     height: auto;
+    flex: 8;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
   }
@@ -34,19 +34,20 @@ const TagContainer = styled.span`
 interface PropsType {
   name: string;
   emoji: string;
-  onChange: object;
   register: any;
+  disabled: boolean;
 }
 
-const Tag = ({ onChange, register, name, emoji }: PropsType) => (
+const Tag = ({ disabled, register, name, emoji }: PropsType) => (
   <TagContainer>
     <input
+      disabled={disabled}
       type="checkbox"
-      id={name}
-      name="tags"
+      // id={name}
+      // name="tags"
+      name={name}
       value={name}
-      onChange={onChange}
-      {...register('tags', { required: '태그를 선택하세요' })}
+      {...register('tags', { required: true })}
     />
     <label htmlFor={name}>
       {name}
